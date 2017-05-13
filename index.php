a8595ea16f7f08f48848e8edc4a79655b1a691dd
benedekb97 <benedekb97@gmail.com>
2017-05-14 00:16:47 +0200

    post variables

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
 include('includes/init.php');
+if(isset($_POST['api_user']) && isset($_POST['api_key'])){
+    $api_user = $mysql->real_escape_string($_POST['api_user']);
+    $api_key = $mysql->real_escape_string($_POST['api_key']);
+}
